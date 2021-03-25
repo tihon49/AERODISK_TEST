cf98f4d3977b516be4da5a4fbe4faa201124a6ea
tihon49 <tihon49@gmail.com>
2021-03-25 21:53:54 +0300

    отображение дисков и UI

--- test_proj/main_app/views.py
+++ test_proj/main_app/views.py
@@ -2,7 +2,36 @@ from django.shortcuts import render
 from django.views import View
 
 import subprocess
+import re
 
+
+
+def get_disk_data(some_data: str) -> dict:
+    """принимает строку, ищет в ней:
+            1. Имя диска
+            2. Размер диска
+            3. Mountpoint
+
+        возвращает словарь с данными о диске либо False
+    """
+
+    #находим нужные данные с помощью регулярных выражений
+    disk_name_lookFor = 'sd[a-z]\d?'
+    disk_space_lookFor = '\d{1,4},?\d?[A-Z]'
+
+    disk_name = re.findall(disk_name_lookFor, some_data)
+    disk_space = re.findall(disk_space_lookFor, some_data)
+    mountpoint = some_data.split(' ')[-1]
+
+    if disk_name:
+        data_to_return = ({'name': disk_name[0],
+                           'size': disk_space[0],
+                           'mountpoint': mountpoint
+                            })
+
+        return data_to_return
+
+    return False
 
 
 class BaseView(View):
@@ -14,8 +43,10 @@ class BaseView(View):
         data = subprocess.Popen('lsblk', stdout = subprocess.PIPE, shell = True)
         result = data.communicate()[0].decode('cp866')
 
-        for line in result.split('\n'):
-            context['data'].append(line)
+        for line in result.split('\n')[1:]:
+            result = get_disk_data(line)
+
+            if result:
+                context['data'].append(result)
         
         return render(request, template, context)
-        
